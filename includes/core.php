c4a6b05d68a61dfb8bc096db5b8aa4d3348bad15
sanketio <sanket.parmar11@gmail.com>
2026-02-25 18:39:41 +0530

    Fix script/style loader

--- includes/core.php
+++ includes/core.php
@@ -21,8 +21,6 @@ function setup() {
 	};
 
 	add_action( 'admin_enqueue_scripts', $n( 'admin_styles' ) );
-	add_action( 'wp_enqueue_scripts', $n( 'styles' ) );
-	add_action( 'wp_enqueue_scripts', $n( 'scripts' ) );
 }
 
 /**
@@ -41,51 +39,5 @@ function admin_styles() {
 		CLOCKS_DIST_URL . 'admin-style.css',
 		$admin_style_dependencies,
 		$admin_style_version,
-	);
-}
-
-/**
- * Enqueue styles for frontend.
- *
- * @return void
- */
-function styles() {
-
-	if ( ! has_block( 'world-clocks/clocks' ) ) {
-		return;
-	}
-
-	$frontend_css_data    = get_asset_info( 'frontend' );
-	$frontend_css_version = empty( $frontend_css_data['version'] ) ? constant( 'CLOCKS_VERSION' ) : $frontend_css_data['version'];
-
-	wp_enqueue_style(
-		'world-clocks-frontend-style',
-		CLOCKS_DIST_URL . 'frontend.css',
-		[],
-		$frontend_css_version,
-	);
-}
-
-/**
- * Enqueue scripts for frontend.
- *
- * @return void
- */
-function scripts() {
-
-	if ( ! has_block( 'world-clocks/clocks' ) ) {
-		return;
-	}
-
-	$frontend_js_data         = get_asset_info( 'frontend' );
-	$frontend_js_dependencies = empty( $frontend_js_data['dependencies'] ) ? [] : $frontend_js_data['dependencies'];
-	$frontend_js_version      = empty( $frontend_js_data['version'] ) ? constant( 'CLOCKS_VERSION' ) : $frontend_js_data['version'];
-
-	wp_enqueue_script(
-		'world-clocks-frontend-js',
-		CLOCKS_DIST_URL . 'frontend.js',
-		$frontend_js_dependencies,
-		$frontend_js_version,
-		true
 	);
 }
